bedb9605d10fe890dd59237f47ae6933b24f2f61
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2023-12-19 22:30:12 +0530

    Add files via upload

---
+++ 2620. Counter.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 10;
+console.log(num1 + num2);
+//20 Numbers are added
+
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1 + str2);
+//HelloWorld Strings are joined
